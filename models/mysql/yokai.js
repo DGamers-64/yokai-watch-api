7acc3a8f5a421f6115ca7e929366862df98cc16f
DGamers-64 <dgamers.643@gmail.com>
2024-10-22 19:39:41 +0200

    Configured SQL for the deploy

--- models/mysql/yokai.js
+++ models/mysql/yokai.js
@@ -1,11 +1,12 @@
 import mysql from 'mysql2/promise'
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT } from '../../config.js'
 
 const config = {
-    host: 'localhost',
-    user: 'root',
-    port: 3306,
-    password: '',
-    database: 'yokaidb'
+    host: DB_HOST,
+    user: DB_USER,
+    port: DB_PORT,
+    password: DB_PASSWORD,
+    database: DB_NAME
 }
 
 const connection = await mysql.createConnection(config)
