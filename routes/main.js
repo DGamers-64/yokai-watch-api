bfdce2f7e7a4f14a9b809ecc03c02a891b753efc
DGamers-64 <dgamers.643@gmail.com>
2024-11-24 11:54:51 +0100

    Añadido inventario y visibilidad de todos los bolsillos

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { YokaiController } from '../controllers/yokai.js'
 import { GameController } from '../controllers/game.js'
+import { InventarioController } from '../controllers/inventario.js'
 
 export const mainRouter = Router({ mergeParams: true })
 
@@ -9,3 +10,9 @@ mainRouter.get('/', GameController.getGame)
 mainRouter.get('/yokai', YokaiController.getAllYokai)
 
 mainRouter.get('/yokai/:id', YokaiController.getYokaiByParam)
+
+mainRouter.get('/inventario', InventarioController.getBolsillos)
+
+// mainRouter.get('/inventario/:bolsillo', InventarioController.getItemsInBolsillo)
+
+// mainRouter.get('/inventario/:bolsillo/:id', InventarioController.getItemsInfo)
