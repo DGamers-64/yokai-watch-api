39706c72744e93a512ea5dbc1ce85d2247f6402d
DGamers-64 <dgamers.643@gmail.com>
2025-01-29 05:54:54 +0100

    Arreglado manejo de los filtros

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -21,7 +21,9 @@ export function filterMiddleware(req, res, next) {
         let filtrosConsulta = "1"
         let valoresFiltros = []
 
-        const filtros = req.query.filter.split("|").map(f => {
+        const filtros = Array.isArray(req.query.filter) ? req.query.filter : [req.query.filter]
+
+        const parsedFilters = filtros.map(f => {
             const match = f.match(/^(\w+)([:=<>]+)(.+)$/)
             if (!match) {
                 return { error: `Filtro inválido: ${f}` }
@@ -42,16 +44,16 @@ export function filterMiddleware(req, res, next) {
             return { columna, operador, valor }
         });
 
-        for (const filtro of filtros) {
+        for (const filtro of parsedFilters) {
             if (filtro.error) {
-                return { error: filtro.error }
+                return res.status(400).json({ error: filtro.error })
             }
         }
 
-        filtrosConsulta = filtros.map(f => `?? ${f.operador} ?`).join(" AND ")
-        valoresFiltros = filtros.flatMap(f => [f.columna, f.valor])
+        filtrosConsulta = parsedFilters.map(f => `?? ${f.operador} ?`).join(" AND ")
+        valoresFiltros = parsedFilters.flatMap(f => [f.columna, f.valor])
 
-        req.filtros = { consulta: filtrosConsulta, valores: valoresFiltros}
+        req.filtros = { consulta: filtrosConsulta, valores: valoresFiltros }
     } else {
         req.filtros = { consulta: "? = ?", valores: [1, 1] }
     }
